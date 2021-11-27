b076123237043f6458d425220e689ec08a630ef1
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-11-27 22:16:15 +0100

    Fixed one error, created a new one

--- frontend/kassaapje/src/App.svelte
+++ frontend/kassaapje/src/App.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	export let name: string;
+	export let phrase: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<p>{phrase}</p>
 	<div>
 		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	</div>
 	
 </main>
 
-<style lang="scss">
+<style  lang="scss">
 	
   $color: orange;
 
