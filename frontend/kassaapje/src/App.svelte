dd91d3f5e2a799f5f560b63d46564bef276fa2b7
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-11-27 14:03:08 +0100

    Svelte setup, TS werkt

---
+++ frontend/kassaapje/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<div>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</div>
+	
+</main>
+
+<style lang="scss">
+	
+  $color: orange;
+
+h1 {
+  color: $color;
+}
+
+div {
+  background: green;
+
+  > p {
+	color: #fff;
+  }
+}
+</style>
