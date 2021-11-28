efcbc9bb2ff14b41b4c62ca465e4dd6e47fb3373
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-11-28 10:28:26 +0100

    Basic setup complete

--- frontend/kassaapje/src/App.svelte
+++ frontend/kassaapje/src/App.svelte
@@ -6,7 +6,7 @@
 
 <main>
 	<Sidebar/>
-	<h1>Hello {name}!</h1>
+	<h1>{name}!</h1>
 	<p>{phrase}</p>
 	<div class="phrase">
 		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
