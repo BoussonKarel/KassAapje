e9a21159b206a5a11ecb4c9b8840ee72c65761cb
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-01 11:09:51 +0100

    Ready for list positioning

--- frontend/kassaapje-vite/kassaapje/src/App.svelte
+++ frontend/kassaapje-vite/kassaapje/src/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import './styles/screen.scss'
   import Sidebar from './components/sidebar.svelte'
+
 </script>
 
 <main>
   <Sidebar />
+  
+
 </main>
--- frontend/kassaapje-vite/kassaapje/src/components/sidebar.svelte
+++ frontend/kassaapje-vite/kassaapje/src/components/sidebar.svelte
@@ -1,46 +1,71 @@
 <script lang="ts">
+  import AccountCircleOutline from 'svelte-material-icons/AccountCircleOutline.svelte'
+  import LogoutVariant from 'svelte-material-icons/LogoutVariant.svelte'
+  import ChevronUp from 'svelte-material-icons/ChevronUp.svelte'
+  import ChevronDown from 'svelte-material-icons/ChevronDown.svelte'
+  import ArrowCollapseLeft from 'svelte-material-icons/ArrowCollapseLeft.svelte'
+  import ArrowCollapseRight from 'svelte-material-icons/ArrowCollapseRight.svelte'
+  import ArrowCollapse from 'svelte-material-icons/ArrowCollapse.svelte'
 </script>
 
-<div class="sidebar">
-  <div class="sidebar-title">KassAapje</div>
+<div class="c-sidebar">
+  <div class="c-sidebar-title">KassAapje</div>
 
-  <div class="sidebar-profile">
-    <div class="sidebar-profile-icon">
-      <img src="public/assets/icons/user-circle.svg" alt="" />
+  <div class="c-sidebar-profile">
+    <div class="c-sidebar-profile-icon">
+      <AccountCircleOutline />
     </div>
-    <div class="sidebar-profile-name">Michiel</div>
+    <div class="c-sidebar-profile-name">Michiel</div>
   </div>
 
-  <div class="sidebar-logout">
-    <div class="sidebar-logout-icon">
-      <img src="public/assets/icons/logout.svg" alt="" />
+  <div class="c-sidebar-logout">
+    <div class="c-sidebar-logout-icon">
+      <LogoutVariant />
     </div>
-    <div class="sidebar-logout-text">Uitloggen</div>
+    <div class="c-sidebar-logout-text">Uitloggen</div>
   </div>
 
-  <div class="sidebar-organisations">
-    <div>Verenigingen</div>
-    <ul>
-      <li>Scouts Beselare</li>
+  <div class="c-sidebar-organisations">
+    <div class="c-sidebar-organisations-title">
+      <div class="c-sidebar-organisations-title-text">Verenigingen</div>
+      <div class="c-sidebar-organisations-title-icon">
+        <ChevronDown />
+      </div>
+    </div>
+
+    <ul class="c-sidebar-organisations-list">
+      <div class="c-sidebar-organisations-list-title">
+        <li class="c-sidebar-organisations-list-title-text">Scouts Beselare</li>
+        <div class="c-sidebar-organisations-list-title-icon">
+          <ChevronDown />
+        </div>
+      </div>
+
       <ul>
-        <li>Kerstmarkt</li>
-        <li>Pasta Avond</li>
-        <li>Lorem</li>
-        <li>Ipsum</li>
+        <li class="c-sidebar-organisations-list-item">Kerstmarkt</li>
+        <li class="c-sidebar-organisations-list-item">Pasta Avond</li>
+        <li class="c-sidebar-organisations-list-item">Lorem</li>
+        <li class="c-sidebar-organisations-list-item">Ipsum</li>
       </ul>
-      <li>De Kouter</li>
+      <div class="c-sidebar-organisations-list-title">
+        <li class="c-sidebar-organisations-list-title-text">De Kouter</li>
+        <div class="c-sidebar-organisations-list-title-icon">
+          <ChevronDown />
+        </div>
+      </div>
+
       <ul>
-        <li>Blikkefeest</li>
-        <li>Girls Night</li>
-        <li>Tea Day</li>
+        <li class="c-sidebar-organisations-list-item">Blikkefeest</li>
+        <li class="c-sidebar-organisations-list-item">Girls Night</li>
+        <li class="c-sidebar-organisations-list-item">Tea Day</li>
       </ul>
     </ul>
 
-    <div class="sidebar-collapse">
-      <div class="sidebar-collapse-icon">
-        <img src="public/assets/icons/arrow-collapse.svg" alt="">
+    <div class="c-sidebar-collapse">
+      <div class="c-sidebar-collapse-icon">
+        <ArrowCollapseRight />
       </div>
-      <div class="sidebar-collapse-text"> Verkleinen</div>
+      <div class="c-sidebar-collapse-text">Verkleinen</div>
     </div>
   </div>
 </div>
