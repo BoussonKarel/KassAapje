8a38f927078c597d365c9cac88a1e2e9b916bea3
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-06 16:50:04 +0100

    Cards stationary done (+ some other basic components)
    Pretty messy commit, should split these up in the future

--- frontend/kassaapje-vite/kassaapje/src/App.svelte
+++ frontend/kassaapje-vite/kassaapje/src/App.svelte
@@ -2,14 +2,18 @@
   import './styles/screen.scss'
   import Sidebar from './components/sidebar.svelte'
   import OrganisationOverview from './components/pages/organisationOverview.svelte'
+import AddOrganisation from './components/pages/AddOrganisation.svelte'
 </script>
 
 <main class="c-app">
   <aside class="c-sidebar">
     <Sidebar />
   </aside>
-  <div class="c-organisation-overview">
+  <!-- <div class="c-organisation-overview">
     <OrganisationOverview />
+  </div> -->
+  <div class="c-organisation-add">
+    <AddOrganisation/>
   </div>
   <div />
 </main>
