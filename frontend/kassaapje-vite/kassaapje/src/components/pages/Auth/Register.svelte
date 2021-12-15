ffbd1dd0c1cfa8c9b12df0f4bfd6fd6c28a91675
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-15 12:16:49 +0100

    Registerfield made

---
+++ frontend/kassaapje-vite/kassaapje/src/components/pages/Auth/Register.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+    let email;
+    let password;
+
+    
+
+    const submitRegister = () => {
+        const submission = {
+            email, password
+        }
+        console.log(submission)
+    }
+
+</script>
+
+<form action="register">
+    <label for="email">Email:</label>
+    <input bind:value={email} type="text" name="email" id="email">
+    <label for="password">Wachtwoord:</label>
+    <input bind:value={password} type="password" name="password" id="password">
+
+    <!-- <label for="confirm">Wachtwoord bevestigen</label>
+    <input type="password" name="confirm" id="confirm"> -->
+
+    <button on:click|preventDefault={submitRegister}>
+        registreren
+    </button>
+</form>
