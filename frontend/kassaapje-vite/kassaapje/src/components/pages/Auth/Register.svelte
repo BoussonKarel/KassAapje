94413f8e48d8265402cf86de802f0949fdd00428
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-15 14:43:49 +0100

    Clean user registration from frontend
    no validation, but a user can be created

--- frontend/kassaapje-vite/kassaapje/src/components/pages/Auth/Register.svelte
+++ frontend/kassaapje-vite/kassaapje/src/components/pages/Auth/Register.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { userAPI } from '../../../utils/userAPI'
+  import { userAPI } from '../../../utils/userAPI'
 
   let name
   let email
@@ -9,29 +9,42 @@ import { userAPI } from '../../../utils/userAPI'
     const submission = {
       name,
       password,
-      email
-      
+      email,
     }
-    console.log(submission);
+    console.log(submission)
     userAPI.post.signup(submission)
-
-//     fetch('http://localhost:8888/signup', {method: 'POST', body: JSON.stringify(submission),headers: {"Content-type": "application/json; charset=UTF-8"}}).then(response => response.json())
-// .then(json => console.log(json))
-// .catch(err => console.log(err));
-
   }
 </script>
 
-<form action="register">
-  <label for="name">Naam:</label>
-  <input bind:value={name} type="text" name="name" id="name" />
-  <label for="email">Email:</label>
-  <input bind:value={email} type="text" name="email" id="email" />
-  <label for="password">Wachtwoord:</label>
-  <input bind:value={password} type="password" name="password" id="password" />
-
-  <!-- <label for="confirm">Wachtwoord bevestigen</label>
-    <input type="password" name="confirm" id="confirm"> -->
-
-  <button on:click|preventDefault={submitRegister}> registreren </button>
-</form>
+<div class="c-register">
+  <form action="register" class="c-register-form">
+    <label class="c-form-label" for="name">Naam:</label>
+    <input
+      class="c-form-textinput"
+      bind:value={name}
+      type="text"
+      name="name"
+      id="name"
+      placeholder="Kass Aapje"
+    />
+    <label class="c-form-label" for="email">Email:</label>
+    <input
+      class="c-form-textinput"
+      bind:value={email}
+      type="text"
+      name="email"
+      id="email"
+      placeholder="email@adres.com"
+    />
+    <label class="c-form-label" for="password">Wachtwoord:</label>
+    <input
+      class="c-form-textinput"
+      bind:value={password}
+      type="password"
+      name="password"
+      id="password"
+      placeholder="Wachtwoord"
+    />
+    <button class="c-button-save" on:click|preventDefault={submitRegister}> Registreren </button>
+  </form>
+</div>
