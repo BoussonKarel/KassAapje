2b8d5fda6bd52372ce1c5495aed8a8301270eff8
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-16 20:37:53 +0100

    Add product page added

--- frontend/src/components/pages/Product/AddProduct.svelte
+++ frontend/src/components/pages/Product/AddProduct.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
    import NavigationBar from '../../NavigationBar.svelte'
+
+   async function handleSubmit(event) {
+    console.log('submitted')
+    console.log(event)
+  }
 </script>
 
 <div class="c-page">
    <div class="c-navigation">
       <NavigationBar />
    </div>
-   <h1>Add Product</h1>
+   <form class="c-form" name="AddOrganisation" on:submit|preventDefault={handleSubmit}>
+    <div class="c-form-textinputs">
+      <label class="c-form-label" for="Name"> Naam: </label>
+      <input class="c-form-textinput" type="text" name="Name" placeholder="Naam" />
+      <label class="c-form-label" for="Website"> Hoeveelheid: </label>
+      <input class="c-form-numberinput" type="number" name="Website" placeholder=1 />
+
+      <button class="c-button-save"> Opslaan </button>
+    </div>
+    
+     
+
+     
+
+     
+    
+  </form>
 </div>
