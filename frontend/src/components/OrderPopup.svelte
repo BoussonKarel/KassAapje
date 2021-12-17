eff591611e16d1d1c875cbffdccb114aecbdbdd0
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-17 16:52:23 +0100

    Popup v1 done
    Responsive for most screen sizes, a little more testing needed though

--- frontend/src/components/OrderPopup.svelte
+++ frontend/src/components/OrderPopup.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
+import { useDeviceLanguage } from "@firebase/auth";
+import PopupTable from "./PopupTable.svelte";
+
 
 </script>
 
 <div class="c-popup-order">
-    <p class="c-popup-order-title">
+    <p class="c-popup-order__title">
         Bestelling afronden
     </p>
 
-    <div>
-        betaling
-    </div>
-    <div>
-        products
-    </div>
-    <div>totaal</div>
-    <button class="c-button-save">Bevestigen</button>
+   
+    <PopupTable/>
+
+     <div class="u-toolbar-amount c-popup-order__amount">
+         <div class="u-toolbar-amount__title">Totaal:</div>
+         <div class="u-horizontal">
+          <div class="u-toolbar-amount__symbol">€</div>
+          <div class="u-toolbar-amount__number">420,60</div>
+         </div>
+         
+      </div>
+    <button class="c-button-save u-popup-submit">Bevestigen</button>
     
   </div>
---
+++ frontend/src/components/PopupTable.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import PopupTableItem from "./PopupTableItem.svelte";
+
+</script>
+
+<div class="c-table--finish-order">
+
+
+
+<PopupTableItem/>
+<PopupTableItem/>
+<PopupTableItem/>
+<PopupTableItem/>
+
+
+
+
+
+
+</div>
