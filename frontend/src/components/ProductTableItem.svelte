156dc5bfe140271f3450adb8eb396c41f1e16650
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-17 11:56:42 +0100

    Product flow in place

---
+++ frontend/src/components/ProductTableItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="c-table__item">
+   <div>Appeljenever</div>
+
+   <div class="c-table__item--price">
+      <div>€</div>
+      <div>40</div>
+   </div>
+</div>
