0e145f5f98e655691cb0a6dd3484ed8ce5c48676
Karel <karel.bousson@student.howest.be>
2021-12-17 12:19:29 +0100

    .

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,20 +3,19 @@
    import { Router, Route } from "svelte-navigator";
 
    import { auth } from './utils/auth'
-import Register from './components/pages/Auth/Register.svelte';
-import Login from './components/pages/Auth/Login.svelte';
-import Sidebar from './components/Sidebar.svelte';
-import OrganisationOverview from './components/pages/Organisation/OrganisationOverview.svelte';
-import AddOrganisation from './components/pages/Organisation/AddOrganisation.svelte';
-import OrganisationInfo from './components/pages/Organisation/organisationInfo.svelte';
-import OrderOverview from './components/pages/Order/OrderOverview.svelte';
-import RegisterInfo from './components/pages/Register/RegisterInfo.svelte';
-import ProductOverview from './components/pages/Product/ProductOverview.svelte';
-import AddProduct from './components/pages/Product/AddProduct.svelte';
-import RegisterOverview from './components/pages/Register/RegisterOverview.svelte';
+   import Register from './components/pages/Auth/Register.svelte';
+   import Login from './components/pages/Auth/Login.svelte';
+   import Sidebar from './components/Sidebar.svelte';
+   import OrganisationOverview from './components/pages/Organisation/OrganisationOverview.svelte';
+   import AddOrganisation from './components/pages/Organisation/AddOrganisation.svelte';
+   import OrganisationInfo from './components/pages/Organisation/organisationInfo.svelte';
+   import OrderOverview from './components/pages/Order/OrderOverview.svelte';
+   import RegisterInfo from './components/pages/Register/RegisterInfo.svelte';
+   import ProductOverview from './components/pages/Product/ProductOverview.svelte';
+   import AddProduct from './components/pages/Product/AddProduct.svelte';
+   import RegisterOverview from './components/pages/Register/RegisterOverview.svelte';
 
    let loggedIn = () => (auth.currentUser ? true : false)
-   // let loggedIn = false
 </script>
 
 <main class="c-app">
