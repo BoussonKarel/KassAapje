89120eea4b114a7956e0011c554abd059b3e68e4
Ibe Verbeke <ibe.verbeke@student.howest.be>
2021-12-22 10:48:53 +0100

    filler text removal
    Removing some of the ridiculous filler text to test overflow

--- frontend/src/components/Cards/RegisterCard.svelte
+++ frontend/src/components/Cards/RegisterCard.svelte
@@ -13,13 +13,13 @@
   </div>
 
   <div class="c-registercard__info">
-    <div class="c-registercard__info--desc">Kassa voor de kerstmarkt 2021 met heel veel adjes voor de sfeer</div>
-    <div class="c-registercard__info--org">Scouts Beselare waar kinderen komen om zich te amuseren enzo boysss</div>
+    <div class="c-registercard__info--desc">Kassa voor de kerstmarkt 2021r</div>
+    <div class="c-registercard__info--org">Scouts Beselare</div>
     <div class="c-registercard__info--admin">
       <div class="c-registercard__icon-star">
         <Star />
       </div>
-      <div class="c-registercard__info--admin-name">Michiel De Roovere die wel nog cool is enzo gekke man swag 4000</div>
+      <div class="c-registercard__info--admin-name">Michiel De Roovere</div>
     </div>
   </div>
 </div>
