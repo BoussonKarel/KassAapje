6cdef1e6eb6a4ae795a97ca86cef6e74d35e91e2
Karel <karel.bousson@student.howest.be>
2021-12-24 11:32:15 +0100

    loading indicator start

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -8,16 +8,16 @@
    import AddOrganisation from './components/pages/Organisation/AddOrganisation.svelte'
    import { onMount } from 'svelte'
    import { useNavigate } from 'svelte-navigator'
-   import OrganizationRoutes from './components/OrganizationRoutes.svelte';
-   import { getAuth } from 'firebase/auth';
-   import { authStore, setAuthStore } from './utils/auth';
+   import OrganizationRoutes from './components/OrganizationRoutes.svelte'
+   import { getAuth } from 'firebase/auth'
+   import { authStore, setAuthStore } from './utils/auth'
+   import PageLoading from './components/Loading/PageLoading.svelte';
 
    onMount(() => {
       getAuth().onAuthStateChanged(async user => {
          if (user) {
             setAuthStore(user)
-         }
-         else {
+         } else {
             authStore.set(null)
          }
       })
@@ -28,37 +28,41 @@
       await getAuth().signOut()
       navigate('/')
    }
+
+   $: loading = false;
 </script>
 
 <main class="c-app">
-   <Router>
-      {#if !$authStore}
-         <Route>
-            <Login />
-         </Route>
-         <Route path="/register">
-            <Register />
-         </Route>
-         <!-- <Route path="/login">
+   <PageLoading loading={loading}>
+      <Router>
+         {#if !$authStore}
+            <Route>
+               <Login />
+            </Route>
+            <Route path="/register">
+               <Register />
+            </Route>
+            <!-- <Route path="/login">
             {(() => {
                const navigate = useNavigate()
                navigate('/')
             })()}
          </Route> -->
-      {:else}
-         <Sidebar />
-         <Route path="/">
-            <OrganisationOverview />
-         </Route>
-         <Route path="/new">
-            <AddOrganisation />
-         </Route>
-         <Route path="/signout">
-            {signOut()}
-         </Route>
-         <Route path="/:orgId/*" let:params>
-            <OrganizationRoutes />
-         </Route>
-      {/if}
-   </Router>
+         {:else}
+            <Sidebar />
+            <Route path="/">
+               <OrganisationOverview />
+            </Route>
+            <Route path="/new">
+               <AddOrganisation />
+            </Route>
+            <Route path="/signout">
+               {signOut()}
+            </Route>
+            <Route path="/:orgId/*" let:params>
+               <OrganizationRoutes />
+            </Route>
+         {/if}
+      </Router>
+   </PageLoading>
 </main>
---
+++ frontend/src/components/Loading/PageLoading.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Loading from "./Loading.svelte";
+  export let loading = false;
+</script>
+
+{#if loading}
+  <Loading size={2} />
+{/if}
+<slot />
