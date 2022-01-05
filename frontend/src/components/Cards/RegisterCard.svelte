a0747c7a5ba77b13566a135ace3f59413007ed75
Karel <karel.bousson@student.howest.be>
2022-01-05 12:01:41 +0100

    kassa card changes

--- frontend/src/components/Cards/RegisterCard.svelte
+++ frontend/src/components/Cards/RegisterCard.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import { authStore } from '../../utils/auth';
+
+  import { onMount } from 'svelte';
+
   import Settings from 'svelte-material-icons/Settings.svelte'
   import Star from 'svelte-material-icons/Star.svelte'
   import { Link } from 'svelte-navigator';
@@ -19,12 +23,6 @@
     <div class="c-registercard__info">
       <div class="c-registercard__info--desc">{register.description}</div>
       <div class="c-registercard__info--org">{register.organization.name}</div>
-      <div class="c-registercard__info--admin">
-        <div class="c-registercard__icon-star">
-          <Star />
-        </div>
-        <div class="c-registercard__info--admin-name">Michiel De Roovere</div>
-      </div>
     </div>
   </div>
 </Link>
