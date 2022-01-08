2b4c85c0fabe9565ab2c7d2d609dd9f6d0222b5b
Karel <karel.bousson@student.howest.be>
2022-01-08 21:43:32 +0100

    unnecessary ignore

---
+++ frontend/src/config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmPq9osI-a4PxpJSef0x18hVssTIotiNI",
+  authDomain: "kassaapje-auth.firebaseapp.com",
+  projectId: "kassaapje-auth",
+  storageBucket: "kassaapje-auth.appspot.com",
+  messagingSenderId: "832367682286",
+  appId: "1:832367682286:web:0933770bfabf91a72a89ff"
+}
